ab6636240ac11b2a256ce8c22c686e9202a221e2
marcdelgado <marcdelgado@gmail.com>
2024-05-06 13:38:46 +0200

    regeneracions literales y galeria

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -13,12 +13,14 @@
 
 /* https://godofredo.ninja/como-detectar-un-elemento-sticky-cuando-se-queda-fijo/ */
 document.addEventListener('DOMContentLoaded', function () {
-  const main_header = document.querySelector('.header-layout');
+  const main_header = document.querySelector('.header-layout__menu-hr');
 
-  const observer = new IntersectionObserver(
-    ([e]) => e.target.classList.toggle("sticked", e.intersectionRatio < 1),
+  /*const observer = new IntersectionObserver(
+    ([e]) => e.target.classList.toggle("fixed", e.intersectionRatio < 1),
     { threshold: [1] }
-  );
+  );*/
+
+
   observer.observe(main_header);
 
 });
@@ -28,3 +30,16 @@ function switchHamburger() {
   document.querySelector('.header-layout__main-header__hamburger-icon').classList.toggle("deployed");
   document.querySelector('.header-layout__main-header__menu-box').classList.toggle("deployed");
 }
+
+
+function checkStickyMenu() {
+  const header = document.querySelector('.header-layout');
+  if (window.scrollY > 41) {
+    header.classList.add('header-layout--fixed');
+  } else {
+    header.classList.remove('header-layout--fixed');
+  }
+}
+
+// Añade el listener al evento scroll de la ventana
+window.addEventListener('scroll', checkStickyMenu);
